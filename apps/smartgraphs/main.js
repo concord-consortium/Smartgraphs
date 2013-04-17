f1d9f603e1a025ab7307517a2dc6a1894b086347
Parker Morse <pmorse@concord.org>
2013-04-17 13:57:49 -0400

    Removes the onbeforeunload handler at end of activity
    [#46624727] At the same time as we send the "Activity completed" action to Google Analytics, we'll also turn off the handler that posts the warning alert, so you can reload/leave then without a warning.

--- apps/smartgraphs/main.js
+++ apps/smartgraphs/main.js
@@ -31,6 +31,7 @@ Smartgraphs.main = function main() {
 
   // prevent unintended reload or back button; use 'onbeforeunload' syntax rather than $.bind just to be sure
   // there's only one handler (and $.bind doesn't really try to normalize this handler anyway)
+  // This handler is removed when the activity is finished
   window.onbeforeunload = function () {
     return "You will lose your place in the activity if you leave this page.";
   };
