7780c065381c3fb3be20647624cea13bf1a6db46
Richard Klancer <rpk@pobox.com>
2013-06-06 16:38:48 -0400

    Fix Jasmine helper definitions.
    Jasmine matchers defined by adding a method to Jasmine.Matchers.prototype never actually fail, leading to possible false positives.

--- apps/smartgraphs/debug/jasmine_helpers.js
+++ apps/smartgraphs/debug/jasmine_helpers.js
@@ -1,23 +1,44 @@
-/*globals jasmine describe it expect xit xdescribe beforeEach afterEach spyOn runs waits waitsFor clickOn fillIn */
+/*global beforeEach, afterEach */
 
-function defineJasmineHelpers() {
+window.defineJasmineHelpers = function() {
 
-  jasmine.Matchers.prototype.toBeA = function (scType) {
-    return SC.kindOf(this.actual, scType);
-  };
+  beforeEach(function() {
+    this.addMatchers({
+      toBeA: function(scType) {
+        return SC.kindOf(this.actual, scType);
+      },
 
-  jasmine.Matchers.prototype.toContainA = function (scType) {
-    var contains = function (array, scType) {
-      if (!array) return false;
-      var el = array.shift();
-      return SC.kindOf(el, scType) || contains(array, scType);
-    };
-    return contains(this.actual, scType);
-  };
+      toContainA: function(scType) {
+        var contains = function (array, scType) {
+          if (!array) return false;
+          var el = array.shift();
+          return SC.kindOf(el, scType) || contains(array, scType);
+        };
+        return contains(this.actual, scType);
+      },
 
-  jasmine.Matchers.prototype.toBeEmpty = function () {
-    return typeof this.actual !== 'undefined' && this.actual.get('length') === 0;
-  };
+      toBeEmpty: function() {
+        return typeof this.actual !== 'undefined' && this.actual.get('length') === 0;
+      },
+
+      toEqualPairs: function() {
+        var i, len, pairs = arguments;
+
+        if ((len = pairs.length) !== this.actual.length) {
+          return false;
+        }
+        for (i = 0; i < len; i++) {
+          if (pairs[i].length !== 2 || this.actual[i].length !== 2) {
+            return false;
+          }
+          if (pairs[i][0] !== this.actual[i][0] || pairs[i][1] !== this.actual[i][1]) {
+            return false;
+          }
+        }
+        return true;
+      }
+    });
+  });
 
   window.runBeforeEach = function (fn) {
     beforeEach( function () { SC.run(fn); });
@@ -26,5 +47,4 @@ function defineJasmineHelpers() {
   window.runAfterEach = function (fn) {
     afterEach( function () { SC.run(fn); });
   };
-
-}
+};
