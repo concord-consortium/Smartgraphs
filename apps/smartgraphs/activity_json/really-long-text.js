bd6483c0525984d1249acdb1685cecc07c26ca94
Richard Klancer <rpk@pobox.com>
2013-12-04 11:05:43 -0500

    Add test activity for scrollable instructions view

---
+++ apps/smartgraphs/activity_json/really-long-text.js
@@ -0,0 +1,169 @@
+Smartgraphs.activityDocs = Smartgraphs.activityDocs || {};
+Smartgraphs.activityDocs["/shared/really-long-text"] =
+{
+  "_id": "really-long-text.df6",
+  "_rev": 1,
+  "data_format_version": 6,
+  "activity": {
+    "title": "Really long text",
+    "url": "/shared/really-long-text",
+    "owner": "shared",
+    "pages": [
+      "/shared/really-long-text/page/1-1-the-page-with-lots-of-text"
+    ],
+    "axes": [],
+    "authorName": "Parker Morse"
+  },
+  "pages": [
+    {
+      "name": "1 The page with lots of text",
+      "url": "/shared/really-long-text/page/1-1-the-page-with-lots-of-text",
+      "activity": "/shared/really-long-text",
+      "index": 1,
+      "introText": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus dui augue, sagittis sed elit tristique, ultrices sagittis lorem. Suspendisse lacus orci, dignissim a porttitor nec, aliquam venenatis orci. In fermentum fermentum diam sit amet aliquam. Donec sed adipiscing mi, id posuere tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras non massa egestas felis lacinia pellentesque et in turpis. Maecenas et volutpat tellus.\r\nDonec venenatis ultrices enim at congue. Fusce iaculis bibendum magna, ac pellentesque enim mollis sed. Donec eleifend est nec sollicitudin ornare. Quisque ac mi ac augue blandit eleifend. Aliquam luctus eros et dolor fringilla, vel fermentum augue blandit. Donec varius, erat vel lobortis aliquet, neque arcu aliquam enim, sit amet venenatis justo justo non diam. Nunc sit amet augue dolor.\r\nPellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam aliquet, lorem non sagittis accumsan, turpis quam malesuada nulla, et porta turpis dolor eget massa. Nunc et tristique eros. Ut vitae lacinia tellus. Nam mattis et diam id ultrices. Integer varius urna lectus, a feugiat mauris aliquam quis. Integer lobortis orci ornare, dapibus massa in, sodales sapien. Vivamus sem quam, tincidunt quis venenatis at, faucibus at dui. Maecenas sed metus laoreet, varius leo at, vehicula dolor.\r\nNulla congue elit dictum, eleifend massa eu, convallis odio. Etiam est ligula, dapibus ac laoreet non, mattis fermentum lorem. Nulla cursus rhoncus dignissim. Nullam pretium faucibus ligula, in interdum purus tempus ut. Fusce interdum aliquet elit. Praesent ac placerat lacus. Duis in volutpat sapien, non faucibus lacus. Curabitur a risus non eros hendrerit feugiat quis vel dolor. Morbi sed fermentum arcu. Mauris non sapien pretium, imperdiet mauris quis, dignissim felis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque gravida arcu in vulputate hendrerit.\r\nMaecenas tortor mauris, euismod sed elit a, viverra venenatis magna. Quisque lacinia tempor facilisis. Nulla facilisi. Aliquam et urna metus. In ut metus sollicitudin velit tempus facilisis non at dui. Nullam pulvinar vel ipsum ac dignissim. Proin ullamcorper magna id velit lobortis consectetur. Aenean tristique a nibh scelerisque tempor. Vestibulum vitae libero dui. Donec non mattis massa. Aenean faucibus iaculis tempor. Nam egestas laoreet mollis. Nunc nec facilisis mi. Sed ac neque fringilla, tincidunt dolor id, euismod libero. Sed scelerisque egestas aliquet. Etiam sed suscipit nunc, ac feugiat nibh.\r\nNulla at molestie lacus, ac ullamcorper sapien. Donec sollicitudin arcu sed diam adipiscing ultrices. Morbi eget blandit lacus, nec viverra sem. Aenean bibendum non nulla sit amet hendrerit. Cras interdum lacinia condimentum. Praesent pretium, sapien sed eleifend vestibulum, quam lacus auctor lorem, vitae elementum lorem tortor at lectus. Etiam malesuada felis et dui ultricies, vel accumsan lorem tincidunt.\r\nQuisque luctus, quam vitae tristique dapibus, nibh tortor porta metus, vitae placerat ante sapien quis libero. Nulla egestas velit sed enim porta, vel commodo sapien volutpat. Aliquam bibendum eget orci quis euismod. Praesent lectus dui, luctus quis elit vitae, interdum commodo nulla. Integer feugiat orci quis facilisis commodo. Nulla ut magna at velit rhoncus commodo vitae et orci. Nam sed nulla risus. Proin bibendum nisl nec vestibulum vulputate. Nam convallis cursus lorem eget luctus.\r\nPhasellus at purus eget massa mattis malesuada in vel quam. Mauris hendrerit diam dignissim erat euismod, ut malesuada mi pulvinar. Praesent convallis quam leo, vel mollis tellus eleifend non. Integer feugiat vitae massa id luctus. Vestibulum auctor egestas interdum. Nam faucibus enim massa, ut consectetur ligula congue id. Nullam justo enim, pellentesque sit amet urna feugiat, commodo mollis leo. Nullam sit amet pulvinar metus. Proin porta tincidunt turpis, ac egestas velit lacinia in.\r\n",
+      "steps": [
+        "/shared/really-long-text/page/1-1-the-page-with-lots-of-text/step/1"
+      ],
+      "firstStep": "/shared/really-long-text/page/1-1-the-page-with-lots-of-text/step/1",
+      "contextVars": []
+    }
+  ],
+  "steps": [
+    {
+      "url": "/shared/really-long-text/page/1-1-the-page-with-lots-of-text/step/1",
+      "activityPage": "/shared/really-long-text/page/1-1-the-page-with-lots-of-text",
+      "beforeText": "<p>This is instruction text which is supposed to appear under the page text.</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eget urna tincidunt turpis venenatis bibendum a a diam. Sed dui sem, tristique eget viverra eu, dignissim eget elit. Aenean fringilla tellus a justo condimentum consequat. Nunc euismod tempus vestibulum. Pellentesque iaculis posuere ante in sollicitudin. Nullam malesuada risus eget nibh dictum imperdiet. Donec risus ante, semper vel blandit vitae, luctus sed risus. Quisque ut nunc nisi. Maecenas dignissim, sapien convallis vestibulum pellentesque, tellus metus venenatis magna, eget faucibus odio risus eget quam. Cras hendrerit iaculis nisl eu pellentesque. Duis convallis viverra ligula, at convallis nisi porttitor id. Cras nec nibh sagittis, consequat justo et, viverra tortor. Aliquam facilisis mollis nisl nec scelerisque. Quisque a diam tellus.</p> <p>Maecenas rhoncus vitae nisi sed lacinia. Cras eu viverra diam. Ut laoreet porta tortor, vitae aliquam felis vehicula sed. Proin elementum mi at orci posuere facilisis. Curabitur sit amet sodales purus. Morbi eu gravida lorem. Proin sed sodales leo, ac tincidunt nunc. Duis id tincidunt metus. Suspendisse sit amet urna vestibulum, lacinia ante vel, fringilla purus. Phasellus quis consequat leo, facilisis pharetra libero. Sed felis est, ornare sed porta non, pretium eget purus. Fusce posuere ipsum nunc, eu varius mi euismod quis. Nunc vel velit sit amet odio tristique blandit.</p>",
+      "paneConfig": "single",
+      "panes": null,
+      "isFinalStep": true,
+      "nextButtonShouldSubmit": true
+    }
+  ],
+  "responseTemplates": [],
+  "axes": [],
+  "datadefs": [],
+  "tags": [],
+  "annotations": [],
+  "variables": [],
+  "units": [
+    {
+      "url": "/shared/really-long-text/units/Time",
+      "activity": "/shared/really-long-text",
+      "name": "Time",
+      "abbreviation": "s",
+      "pluralName": "Time"
+    },
+    {
+      "url": "/shared/really-long-text/units/Distance",
+      "activity": "/shared/really-long-text",
+      "name": "Distance",
+      "abbreviation": "m",
+      "pluralName": "Distance"
+    },
+    {
+      "url": "/shared/really-long-text/units/degrees Celsius",
+      "activity": "/shared/really-long-text",
+      "name": "degrees Celsiu",
+      "abbreviation": "deg C",
+      "pluralName": "degrees Celsius"
+    },
+    {
+      "url": "/shared/really-long-text/units/minutes",
+      "activity": "/shared/really-long-text",
+      "name": "minute",
+      "abbreviation": "min.",
+      "pluralName": "minutes"
+    },
+    {
+      "url": "/shared/really-long-text/units/meters",
+      "activity": "/shared/really-long-text",
+      "name": "meter",
+      "abbreviation": "m",
+      "pluralName": "meters"
+    },
+    {
+      "url": "/shared/really-long-text/units/seconds",
+      "activity": "/shared/really-long-text",
+      "name": "second",
+      "abbreviation": "s",
+      "pluralName": "seconds"
+    },
+    {
+      "url": "/shared/really-long-text/units/centimeters",
+      "activity": "/shared/really-long-text",
+      "name": "centimeter",
+      "abbreviation": "cm",
+      "pluralName": "centimeters"
+    },
+    {
+      "url": "/shared/really-long-text/units/grams",
+      "activity": "/shared/really-long-text",
+      "name": "gram",
+      "abbreviation": "gr",
+      "pluralName": "grams"
+    },
+    {
+      "url": "/shared/really-long-text/units/miles",
+      "activity": "/shared/really-long-text",
+      "name": "mile",
+      "abbreviation": "mi",
+      "pluralName": "miles"
+    },
+    {
+      "url": "/shared/really-long-text/units/hours",
+      "activity": "/shared/really-long-text",
+      "name": "hour",
+      "abbreviation": "hr",
+      "pluralName": "hours"
+    },
+    {
+      "url": "/shared/really-long-text/units/Celsius",
+      "activity": "/shared/really-long-text",
+      "name": "Celsiu",
+      "abbreviation": "° C.",
+      "pluralName": "Celsius"
+    },
+    {
+      "url": "/shared/really-long-text/units/dollars",
+      "activity": "/shared/really-long-text",
+      "name": "dollar",
+      "abbreviation": "$",
+      "pluralName": "dollars"
+    },
+    {
+      "url": "/shared/really-long-text/units/inches",
+      "activity": "/shared/really-long-text",
+      "name": "inche",
+      "abbreviation": "in",
+      "pluralName": "inches"
+    },
+    {
+      "url": "/shared/really-long-text/units/years",
+      "activity": "/shared/really-long-text",
+      "name": "year",
+      "abbreviation": "yr",
+      "pluralName": "years"
+    },
+    {
+      "url": "/shared/really-long-text/units/millimeters",
+      "activity": "/shared/really-long-text",
+      "name": "millimeter",
+      "abbreviation": "mm",
+      "pluralName": "millimeters"
+    },
+    {
+      "url": "/shared/really-long-text/units/parts per million",
+      "activity": "/shared/really-long-text",
+      "name": "parts per million",
+      "abbreviation": "ppm",
+      "pluralName": "parts per million"
+    },
+    {
+      "url": "/shared/really-long-text/units/meters per second",
+      "activity": "/shared/really-long-text",
+      "name": "meters per second",
+      "abbreviation": "m/s",
+      "pluralName": "meters per second"
+    }
+  ]
+}
