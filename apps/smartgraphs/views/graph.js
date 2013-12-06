2e1fb15eb45e5a54c513c5099d9d2a8413d2bb1b
Richard Klancer <rpk@pobox.com>
2013-12-06 12:50:35 -0500

    rm unnecessary mousemoved handler on tooltipView
    [#61991698]

--- apps/smartgraphs/views/graph.js
+++ apps/smartgraphs/views/graph.js
@@ -393,23 +393,6 @@ Smartgraphs.GraphView = SC.View.extend(
           coords.x, ",&nbsp;", coords.y,
         "</div>"
       ].join(''));
-    },
-
-    mouseMoved: function (evt) {
-      var graphCanvasView = this.parentView.graphCanvasView;
-      var graphController = this.parentView.graphController;
-
-      var coords = graphCanvasView.axesView.inputAreaView.coordsForEvent(evt),
-          point = this.parentView.pointForCoordinates(coords.x, coords.y);
-
-      var bounds = graphCanvasView._getScreenBounds();
-
-      var padding = this.parentView.get("padding");
-      if ((graphController.tooltipCoords.width + coords.x) >= (bounds.xRight - padding.right)) {
-        coords.x = bounds.xRight - graphController.tooltipCoords.width - padding.right;
-      }
-      graphController.updateToolTip(point, coords);
-      return;
     }
   }),
 
