2829d86aa96d8f93702bee26d90c19bf68c0e1a0
Noah Paessel <knowuh@gmail.com>
2014-03-28 12:41:35 -0400

    Clean-up Mathjax sample activity ( "#/shared/mathjax" ).
    The previous version had a lot of copy-cruft from Maria's run which
    would certainly have caused confusion.  Also had some poorly formatted
    latex.

--- apps/smartgraphs/activity_json/mathjax.js
+++ apps/smartgraphs/activity_json/mathjax.js
@@ -1,15 +1,15 @@
 Smartgraphs.activityDocs = Smartgraphs.activityDocs || {};
 Smartgraphs.activityDocs["/shared/mathjax"] = {
-  "_id": "maria-ran.df6",
+  "_id": "mathjax.df6",
   "_rev": 1,
   "data_format_version": 6,
   "activity": {
-    "title": "Maria Ran.",
-    "url": "/shared/maria-ran",
+    "title": "Mathjax Test activity",
+    "url": "/shared/mathjax",
     "owner": "shared",
     "pages": [
-      "/shared/maria-ran/page/1-1-introduction",
-      "/shared/maria-ran/page/2-2-where-did-she-stop-testing-1-2-3"
+      "/shared/mathjax/page/1-1-introduction",
+      "/shared/mathjax/page/testing-page-2"
     ],
     "axes": [],
     "authorName": "Bo Jangles."
@@ -17,41 +17,41 @@ Smartgraphs.activityDocs["/shared/mathjax"] = {
   "pages": [
     {
     "name": "1 Introduction",
-    "url": "/shared/maria-ran/page/1-1-introduction",
-    "activity": "/shared/maria-ran",
+    "url": "/shared/mathjax/page/1-1-introduction",
+    "activity": "/shared/mathjax",
     "index": 1,
     "introText": "<p>Here is something bold: &nbsp;<strong>BOLD</strong></p>\r\n<p>Here are sub<sub>script</sub> and super<sup>script</sup></p>\r\n<p>Here is something in Italics: &nbsp;<em>ITALICS</em></p>\r\n<p>Here is a list:</p>\r\n<ol>\r\n<li>a</li>\r\n<li>b</li>\r\n</ol>\r\n<p>A Formula: &nbsp;\\( &nbsp;\\sqrt[2]{y} &nbsp;\\frac{1}{\\prod_{k=1}^n} \\) This is the end of the sentance.</p>",
     "steps": [
-      "/shared/maria-ran/page/1-1-introduction/step/1"
+      "/shared/mathjax/page/1-1-introduction/step/1"
     ],
-    "firstStep": "/shared/maria-ran/page/1-1-introduction/step/1",
+    "firstStep": "/shared/mathjax/page/1-1-introduction/step/1",
     "contextVars": []
   },
   {
-    "name": "2 Where did she stop. Testing 1 2 3",
-    "url": "/shared/maria-ran/page/2-2-where-did-she-stop-testing-1-2-3",
-    "activity": "/shared/maria-ran",
+    "name": "Mathajax testing page 2",
+    "url": "/shared/mathjax/page/testing-page-2",
+    "activity": "/shared/mathjax",
     "index": 2,
-    "introText": "<p>\\( &nbsp;\\sqrt[2]{y} &nbsp;\\frac{1}{\\prod_{k=1}^n} \\)&nbsp;</p>\r\n<p><span class=\"sy0\">\\( \\</span><span class=\"kw1\">frac</span><span class=\"sy0\">{&nbsp;</span><span class=\"re9\">\\<span class=\"kw1\">frac</span><span class=\"sy0\">{</span>1<span class=\"sy0\">}{</span>x<span class=\"sy0\">}&nbsp;</span>+&nbsp;<span class=\"sy0\">\\</span><span class=\"kw1\">frac</span>{1}{y} } {y-z</span><span class=\"sy0\">} \\)</span></p>",
+    "introText": "<p>\\( &nbsp;\\sqrt[2]{y} &nbsp;\\frac{1}{\\prod_{k=1}^n} \\)&nbsp;</p>",
     "steps": [
-      "/shared/maria-ran/page/2-2-where-did-she-stop-testing-1-2-3/step/1"
+      "/shared/mathjax/page/testing-page-2/step/1"
     ],
-    "firstStep": "/shared/maria-ran/page/2-2-where-did-she-stop-testing-1-2-3/step/1",
+    "firstStep": "/shared/mathjax/page/testing-page-2/step/1",
     "contextVars": []
   }
   ],
   "steps": [
     {
-    "url": "/shared/maria-ran/page/1-1-introduction/step/1",
-    "activityPage": "/shared/maria-ran/page/1-1-introduction",
+    "url": "/shared/mathjax/page/1-1-introduction/step/1",
+    "activityPage": "/shared/mathjax/page/1-1-introduction",
     "paneConfig": "single",
     "panes": null,
     "isFinalStep": true,
     "nextButtonShouldSubmit": true
   },
   {
-    "url": "/shared/maria-ran/page/2-2-where-did-she-stop-testing-1-2-3/step/1",
-    "activityPage": "/shared/maria-ran/page/2-2-where-did-she-stop-testing-1-2-3",
+    "url": "/shared/mathjax/page/testing-page-2/step/1",
+    "activityPage": "/shared/mathjax/page/testing-page-2",
     "paneConfig": "single",
     "panes": null,
     "isFinalStep": true,
@@ -66,36 +66,36 @@ Smartgraphs.activityDocs["/shared/mathjax"] = {
   "variables": [],
   "units": [
     {
-    "url": "/shared/maria-ran/units/meters",
-    "activity": "/shared/maria-ran",
+    "url": "/shared/mathjax/units/meters",
+    "activity": "/shared/mathjax",
     "name": "meter",
     "abbreviation": "m",
     "pluralName": "meters"
   },
   {
-    "url": "/shared/maria-ran/units/minutes",
-    "activity": "/shared/maria-ran",
+    "url": "/shared/mathjax/units/minutes",
+    "activity": "/shared/mathjax",
     "name": "minute",
     "abbreviation": "m",
     "pluralName": "minutes"
   },
   {
-    "url": "/shared/maria-ran/units/meters per second",
-    "activity": "/shared/maria-ran",
+    "url": "/shared/mathjax/units/meters per second",
+    "activity": "/shared/mathjax",
     "name": "meters per second",
     "abbreviation": "m/s",
     "pluralName": "meters per second"
   },
   {
-    "url": "/shared/maria-ran/units/seconds",
-    "activity": "/shared/maria-ran",
+    "url": "/shared/mathjax/units/seconds",
+    "activity": "/shared/mathjax",
     "name": "second",
     "abbreviation": "s",
     "pluralName": "seconds"
   },
   {
-    "url": "/shared/maria-ran/units/Flubbers",
-    "activity": "/shared/maria-ran",
+    "url": "/shared/mathjax/units/Flubbers",
+    "activity": "/shared/mathjax",
     "name": "Flubber",
     "abbreviation": "fbs",
     "pluralName": "Flubbers"
